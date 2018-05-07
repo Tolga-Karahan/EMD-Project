0827a92ef472d3ea505ea6ef4828d666bb4d88b0
Tolga Karahan <tkarahan147@gmail.com>
2018-05-07 18:29:31 +0300

    testing

--- Test.java
+++ Test.java
@@ -8,13 +8,18 @@ public class Test
 	{
 		try
 		{
-			BufferedImage input = ImageIO.read(new File("input.jpg"));
+			BufferedImage input = ImageIO.read(new File("input.png"));
 			BufferedImage coverImage = ImageIO.read(new File("cover-image.jpg"));
 
 			Encrypt encryption = new Encrypt(coverImage, input);
 			BufferedImage output = encryption.encrypt();
 
 			ImageIO.write(output, "JPEG", new File("output.jpg"));
+
+			Decrypt decryption = new Decrypt(coverImage, input);
+			BufferedImage secret = decryption.decrypt();
+
+			ImageIO.write(secret, "JPEG", new File("secret.jpg"));
 		}
 		catch(Exception e)
 		{
