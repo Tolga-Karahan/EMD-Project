3cc6de5f6bc6c04c40b91fec68ee3962dda8fbe2
Tolga Karahan <tkarahan147@gmail.com>
2018-05-10 23:20:47 +0300

    some fixes

--- Decrypt.java
+++ Decrypt.java
@@ -1,15 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package emd;
-
-/**
- *
- * @author Tolga
- */
-
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
 import java.awt.image.ColorModel;
