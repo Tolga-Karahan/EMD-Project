c9533a28df2d65ceabb4c0460f0fed6d0e1a9e12
Tolga Karahan <tkarahan147@gmail.com>
2018-05-10 23:59:46 +0300

    some fixes

--- Encrypt.java
+++ Encrypt.java
@@ -65,6 +65,35 @@ public class Encrypt
 		return new BufferedImage(model, coverImage, model.isAlphaPremultiplied(), null); 
 	}
 
+	private void embedPixel(int column, int row, int band)
+	{
+
+		// Satir ve sutun sonu kontrollerinden kurtulmak icin tek boyutta indeksle
+		int index = (row * secretImage.getWidth() + column) * factor;
+
+		// Pikseli al ve sifreleme tabanina cevir
+                int pixel = secretImage.getSample(column, row, band);
+                String cipherPixel = Integer.toString(pixel, base);
+
+                // Gerekiyorsa padding yap
+                if(cipherPixel.length() < Integer.toString(255, base).length())
+                {
+       	        	int diff = Integer.toString(255, base).length() - cipherPixel.length();
+
+                        for(int i = 0; i < diff; i++)
+                        	cipherPixel = '0' + cipherPixel;
+                }
+
+                // Her bir digiti taşıyıcı resme göm
+                for(int i = 0; i < cipherPixel.length(); i++)
+                {
+                	int embeddingDigit = Character.getNumericValue(cipherPixel.charAt(i));
+
+			// Sifreleme tabanindaki her bir digit orten resme gomulur 
+                        embedDigit(embeddingDigit, index + (i * groupSize), band);
+                }
+	}
+	
 	private void embedDigit(int embeddingDigit, int index, int band)
 	{
 		int weightedSum = 0;
@@ -97,35 +126,6 @@ public class Encrypt
 		{
       			coverImage.setSample(column, row, band, coverImage.getSample(column, row, band) - 1);
 		} 
-	}
-
-	private void embedPixel(int column, int row, int band)
-	{
-
-		// Satir ve sutun sonu kontrollerinden kurtulmak icin tek boyutta indeksle
-		int index = (row * secretImage.getWidth() + column) * factor;
-
-		// Pikseli al ve sifreleme tabanina cevir
-                int pixel = secretImage.getSample(column, row, band);
-                String cipherPixel = Integer.toString(pixel, base);
-
-                // Gerekiyorsa padding yap
-                if(cipherPixel.length() < Integer.toString(255, base).length())
-                {
-       	        	int diff = Integer.toString(255, base).length() - cipherPixel.length();
-
-                        for(int i = 0; i < diff; i++)
-                        	cipherPixel = '0' + cipherPixel;
-                }
-
-                // Her bir digiti taşıyıcı resme göm
-                for(int i = 0; i < cipherPixel.length(); i++)
-                {
-                	int embeddingDigit = Character.getNumericValue(cipherPixel.charAt(i));
-
-			// Sifreleme tabanindaki her bir digit orten resme gomulur 
-                        embedDigit(embeddingDigit, index + (i * groupSize), band);
-                }
 	}
 	
 	public double getPSNR()
